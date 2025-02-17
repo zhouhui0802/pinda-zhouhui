94e0883c12e94968fb8a0792ff571f267663421a
zhouhui0802 <1741484908@qq.com>
2025-02-17 09:40:56 +0800

    antisamy-test.xml Demo

---
+++ antiSamy_demo/src/main/java/com/zh/config/AntiSamyConfiguration.java
@@ -0,0 +1,21 @@
+package com.zh.config;
+
+import com.zh.filter.XssFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+public class AntiSamyConfiguration {
+
+    /**
+     * 配置跨站攻击过滤器
+     */
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistration =
+                new FilterRegistrationBean(new XssFilter());
+        filterRegistration.addUrlPatterns("/*");
+        filterRegistration.setOrder(1);
+
+        return filterRegistration;
+    }
+}
